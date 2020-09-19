106fecca27eedea9ef5b86d790c185aaa14c427c
PitiasF <selamptsegu@gmail.com>
2020-09-19 11:09:50 -0400

    more refactoring....

--- Take-Home/src/test/java/gmail/pitias4work/com/DragandDrop.java
+++ Take-Home/src/test/java/gmail/pitias4work/com/DragandDrop.java
@@ -5,13 +5,14 @@ import static org.testng.Assert.assertEquals;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
 
 import com.library.pitias.Base;
 
 public class DragandDrop extends Base {
 	Logger logger = Logger.getLogger(Base.class);
 
-	
+	@Test
 	public void dragandDrop() {
 
 		try {
