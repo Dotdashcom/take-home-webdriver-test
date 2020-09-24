ef31345407e72620ff9bf1d1c9f9a1bd0d2c1c59
Marionela Tirsina <64120351+marionelat@users.noreply.github.com>
2020-09-24 12:59:02 -0400

    Update OpenInNewTab.java

--- src/test/java/MarionelaTirsina/pages/OpenInNewTab.java
+++ src/test/java/MarionelaTirsina/pages/OpenInNewTab.java
@@ -6,4 +6,5 @@ import org.openqa.selenium.support.FindBy;
 public class OpenInNewTab extends BasePage {
     @FindBy(css = "a[href='/windows/new']")
     public WebElement openNeWindowLink;
+    ///
 }
