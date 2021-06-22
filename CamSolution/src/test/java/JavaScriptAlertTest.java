600089ae08aa3eb1e1132e022fc198217acb1df1
C4mz0r <campbell.lazaruk@gmail.com>
2021-06-22 00:30:35 -0600

    add page and tests for js alerts

---
+++ CamSolution/src/test/java/JavaScriptAlertTest.java
@@ -0,0 +1,96 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import pages.JavaScriptAlert;
+
+public class JavaScriptAlertTest {
+
+	private WebDriver driver = null;
+	
+	@Before
+	public void init() {
+		// TODO:  Move to common place
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+	
+	/*
+	 * Click JS alert and assert message
+	 */
+	@Test
+	public void clickAlertDialog() {
+		
+		JavaScriptAlert alertPage = new JavaScriptAlert(driver);
+		
+		alertPage.clickAlertButton();
+		alertPage.clickOkOnAlertDialog();
+		
+		String result = alertPage.getResultText();
+		assertTrue(result.equals("You successfuly clicked an alert"));
+	}
+	
+	@Test
+	public void clickOkOnConfirmDialog() {
+		
+		JavaScriptAlert alertPage = new JavaScriptAlert(driver);
+		
+		alertPage.clickConfirmButton();
+		alertPage.clickOkOnConfirmDialog();
+		
+		String result = alertPage.getResultText();
+		assertTrue(result.equals("You clicked: Ok"));
+		
+	} 
+	
+	@Test
+	public void clickCancelOnConfirmDialog() {
+		
+		JavaScriptAlert alertPage = new JavaScriptAlert(driver);
+		
+		alertPage.clickConfirmButton();
+		alertPage.clickCancelOnConfirmDialog();
+		
+		String result = alertPage.getResultText();
+		assertTrue(result.equals("You clicked: Cancel"));
+
+	}
+	
+	@Test
+	public void typeAndClickOkOnPromptDialog() {
+		
+		JavaScriptAlert alertPage = new JavaScriptAlert(driver);
+		
+		alertPage.clickPromptButton();
+		alertPage.clickOkOnPromptDialog("Hello World");
+		
+		String result = alertPage.getResultText();
+		assertTrue(result.equals("You entered: Hello World"));
+		
+	}
+	
+	@Test
+	public void clickCancelOnPromptDialog() {
+		
+		JavaScriptAlert alertPage = new JavaScriptAlert(driver);
+		
+		alertPage.clickPromptButton();
+		alertPage.clickCancelOnPromptDialog();
+		
+		String result = alertPage.getResultText();
+		assertTrue(result.equals("You entered: null"));
+		
+	}	
+		
+	@After
+	public void cleanup() {
+		driver.quit();
+	}
+	
+}
+
