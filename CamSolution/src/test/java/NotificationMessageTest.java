e4fce6ab22b7a9ee923952b4b8455c2f0f5126e6
C4mz0r <campbell.lazaruk@gmail.com>
2021-06-21 18:43:14 -0600

    add code and test for notification messages

---
+++ CamSolution/src/test/java/NotificationMessageTest.java
@@ -0,0 +1,56 @@
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import pages.NotificationMessage;
+
+public class NotificationMessageTest {
+
+	private WebDriver driver = null;
+	
+	@Before
+	public void init() {
+		// TODO:  Move to common place
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+	
+	/*
+	 * Click link and get the flash message
+	 */
+	@Test
+	public void clickManyTimesToGenerateMessages() {
+		
+		// Number of times to generate messages
+		final int ATTEMPTS = 10;
+		
+		NotificationMessage messagePage = new NotificationMessage(driver);
+		Set<String> allowedMessages = new HashSet<String>();
+		allowedMessages.add("Action successful\n×");
+		allowedMessages.add("Action Unsuccessful\n×");
+		allowedMessages.add("Action unsuccesful, please try again\n×");
+		
+		
+		for (int i=0; i < ATTEMPTS ; i++) {
+			messagePage.loadNewMessage();
+			String message = messagePage.getMessage();
+			assertTrue(allowedMessages.contains(message));
+		}
+	}
+		
+	@After
+	public void cleanup() {
+		driver.quit();
+	}
+	
+}
+
---
+++ CamSolution/src/test/java/pages/NotificationMessage.java
@@ -0,0 +1,37 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+public class NotificationMessage {
+	
+	private WebDriver driver = null;
+	
+	@FindBy(how=How.LINK_TEXT, using="Click here")
+	private WebElement clickHere;
+	
+	public NotificationMessage(WebDriver driver) {
+		this.driver = driver;
+		this.loadPage();
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void loadNewMessage() {
+		clickHere.click();
+	}
+	
+	public String getMessage() {
+		WebElement flash = driver.findElement(By.id("flash"));
+		return flash.getText();
+	}
+	
+	public void loadPage() {
+		//TODO:  use base url variable
+		driver.get("http://localhost:7080/notification_message_rendered");
+	}
+	
+}
