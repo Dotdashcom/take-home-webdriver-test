ba49af43fc80a3fe3fa2d083b28f5cad280cd82b
PawelMurdza <pawel.murdza@gmail.com>
2022-09-05 14:00:53 -0400

    2nd

---
+++ src/test/java/utils/Constants.java
@@ -0,0 +1,8 @@
+package utils;
+
+public class Constants {
+    public static final String baseURL = "http://localhost:7080";
+    public static final String downloadPath = System.getProperty("user.home")+"/Downloads/some-file.txt";
+    public static final String uploadPath = System.getProperty("user.dir")+"/Files/FileToSend.txt";
+
+}
