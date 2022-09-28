9e2316e8253c1602bb56b2b0165737c51d1698fe
mk-bandi <114434812+mk-bandi@users.noreply.github.com>
2022-09-28 10:51:47 -0400

    Supporting Windows and Mac.

--- src/main/java/com/dotdash/mkb/pageobjects/DownloadPage.java
+++ src/main/java/com/dotdash/mkb/pageobjects/DownloadPage.java
@@ -21,7 +21,13 @@ public class DownloadPage extends BasePage {
     }
 
     public boolean isDownloadedFileLocated() {
-        String downloadedFile = System.getProperty("user.home") + "\\downloads\\some-file.txt";
+        String downloadedFile = "";
+        if(System.getProperty("os.name").toLowerCase().startsWith("windows")) {
+            downloadedFile = System.getProperty("user.home") + "\\downloads\\some-file.txt";
+        } else if (System.getProperty("os.name").toLowerCase().startsWith("mac")) {
+            downloadedFile = System.getProperty("user.home") + "/downloads/some-file.txt";
+        }
+        
         File file = new File(downloadedFile);
         waitUntilFileExists(file);
         return file.exists();
