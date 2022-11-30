2620dd0b6c835552a306853f82359ef140c0622c
Mateus Pasqualini <m.victoria@smart-reporting.com>
2022-11-30 07:53:04 -0300

    changing formatting on tests

--- src/main/resources/dragdropscript.js
+++ src/main/resources/dragdropscript.js
@@ -5,18 +5,24 @@ function createEvent(typeOfEvent) {
         data: {},
         setData: function (key, value) { this.data[key] = value },
         getData: function (key) { return this.data[key]; }
-    }; return event;
-} function dispatchEvent(element, event, transferData) {
+    };
+    return event;
+}
+function dispatchEvent(element, event, transferData) {
     if (transferData !== undefined) { event.dataTransfer = transferData; }
     if (element.dispatchEvent) {
         element.dispatchEvent(event);
-    } else if (element.fireEvent) { element.fireEvent(on + event.type, event); }
-} function simulateHTML5DragAndDrop(element, destination) {
+    } else if (element.fireEvent) {
+        element.fireEvent(on + event.type, event);
+        }
+}
+function simulateHTML5DragAndDrop(element, destination) {
     var dragStartEvent = createEvent('dragstart'); dispatchEvent(element, dragStartEvent);
     var dropEvent = createEvent('drop');
     dispatchEvent(destination, dropEvent, dragStartEvent.dataTransfer);
     var dragEndEvent = createEvent('dragend');
     dispatchEvent(element, dragEndEvent, dropEvent.dataTransfer);
-} var source = arguments[0]; var destination = arguments[1];
+}
+var source = arguments[0]; var destination = arguments[1];
 simulateHTML5DragAndDrop(source, destination);
 
