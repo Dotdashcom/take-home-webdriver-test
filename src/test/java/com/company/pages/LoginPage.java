d6caaedbfb4cc46e5a706e1a62dc1e9d2f2c79d8
Vick <vvladovskiy@gmail.com>
2022-12-13 16:28:58 -0800

    iMac version 1.0

--- src/test/java/com/company/pages/LoginPage.java
+++ src/test/java/com/company/pages/LoginPage.java
@@ -5,8 +5,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import static org.bouncycastle.cms.RecipientId.password;
-
 public class LoginPage {
 
     public LoginPage(){
