2ae0520b09f889a1749f88a4c60d4501290d77f6
Vick Wladowski <vvladovskiy@gmail.com>
2022-12-15 17:15:05 -0800

    My Project Assessment Test

--- src/test/java/com/company/tests/TestCase08DynamicControls.java
+++ src/test/java/com/company/tests/TestCase08DynamicControls.java
@@ -1,90 +1,66 @@
 package com.company.tests;
 
-import com.company.base.TestBase;
 import com.company.utilities.Driver;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.awt.*;
 import java.time.Duration;
+import static org.junit.Assert.assertTrue;
 
 /**
- Test clicks on the Remove Button and uses explicit wait.
- Test asserts that the checkbox is gone.
- Test clicks on Add Button and uses explicit wait.
- Test asserts that the checkbox is present.
- Test clicks on the Enable Button and uses explicit wait.
- Test asserts that the text box is enabled.
- Test clicks on the Disable Button and uses explicit wait.
- Test asserts that the text box is disabled.
+ * Test clicks on the Remove Button and uses explicit wait.
+ * Test asserts that the checkbox is gone.
+ * Test clicks on Add Button and uses explicit wait.
+ * Test asserts that the checkbox is present.
+ * Test clicks on the Enable Button and uses explicit wait.
+ * Test asserts that the text box is enabled.
+ * Test clicks on the Disable Button and uses explicit wait.
+ * Test asserts that the text box is disabled.
  */
 
-public class TestCase08DynamicControls{
+public class TestCase08DynamicControls {
 
     @Test
-    public void dynamicControls(){
+    public void dynamicControls() {
 
         Driver.getDriver().get("http://localhost:7080/dynamic_controls");
 
-        WebElement RemoveAddButton = Driver.getDriver().findElement( By.xpath("//*[@id='checkbox-example']/button"));
-        WebElement CheckBox = Driver.getDriver().findElement( By.xpath("//*[@id='checkbox']"));
+        /*
+         Test clicks on the Remove Button and uses explicit wait.
+         Test asserts that the checkbox is gone.*/
 
-        WebElement EnableDisableButton = Driver.getDriver().findElement( By.xpath("//*[@id='input-example']/button"));
+        WebElement RemoveAddButton = Driver.getDriver().findElement(By.xpath("//*[@id='checkbox-example']/button"));
+        WebElement CheckBox = Driver.getDriver().findElement(By.xpath("//*[@id='checkbox']"));
+        assertTrue(CheckBox.isDisplayed());
 
         RemoveAddButton.click();
 
         WebDriverWait wait = new WebDriverWait(Driver.getDriver(),Duration.ofSeconds(10));
-        wait.until(
-                ExpectedConditions.invisibilityOf(CheckBox));
+        wait.until(ExpectedConditions.invisibilityOf(CheckBox));
 
-        Assert.assertFalse(CheckBox.isSelected(), "Checkbox is gone");
+        /*
+         Test clicks on Add Button and uses explicit wait.
+         Test asserts that the checkbox is present.
+         */
+        RemoveAddButton.click();
 
-        Driver.closeDriver();
-    }
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='checkbox']")));
 
-    @Test
-    public void dynamicContent() {
+        WebElement EnableDisableButton = Driver.getDriver().findElement(By.xpath("//*[@id='input-example']/button"));
 
-        Driver.getDriver().get("https://the-internet.herokuapp.com/dynamic_controls");
+        WebElement inputBox = Driver.getDriver().findElement(By.xpath("//*[@id='input-example']/input"));
+        assertTrue(inputBox.isDisplayed());
 
-        // click on enable
-        Driver.getDriver().findElement(By.xpath("//form[@id='input-example']//button")).click();
-        // WebDriverWait  class used to explicit waits
-        // just creating object, waiting does not happen yet
-        WebDriverWait wait = new WebDriverWait(Driver.getDriver(),Duration.ofSeconds(4));
-        // wait.until() : this is when wait happens
-        // we are waiting for certain element this xpath to be clickable
-        wait.until(
-                ExpectedConditions.elementToBeClickable(
-                        By.xpath("//input[@type='text']")));
-        // enter text
-        Driver.getDriver().findElement(By.xpath("//input[@type='text']")).sendKeys("hello world");
+        EnableDisableButton.click();
+        /* Enabled */
+        wait.until(ExpectedConditions.elementSelectionStateToBe(inputBox, inputBox.isEnabled()));
+
+        EnableDisableButton.click();
+        /* Disabled */
+        wait.until(ExpectedConditions.attributeToBeNotEmpty(inputBox,"disabled"));
 
         Driver.closeDriver();
     }
-
-    @Test
-    public void implicitWait2(){
-        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        Driver.getDriver().get("https://the-internet.herokuapp.com/dynamic_controls");
-        // click on remove button
-        Driver.getDriver().findElement(By.cssSelector("form#checkbox-example>button")).click();
-        // print the message
-        System.out.println(Driver.getDriver().findElement(By.id("message")).getText());
-
-        // click on add button
-        Driver.getDriver().findElement(By.cssSelector("form#checkbox-example>button")).click();
-        // verify checkbox is displayed
-        System.out.println(Driver.getDriver().findElement(By.id("checkbox")).isDisplayed());
-
-        Driver.closeDriver();
-    }
-
 }
