34cd6fbd074400397d83299ac39bd7f89e2ff921
Deepika Solai <deepikasolai4410@gmail.com>
2022-12-30 17:31:24 -0500

    Updated Drag and Drop

--- src/test/java/com/takehome/testcases/DragandDropTest.java
+++ src/test/java/com/takehome/testcases/DragandDropTest.java
@@ -13,37 +13,37 @@ public class DragandDropTest {
 
 	DriverScript driverSetup;
 	DragandDropCheck page;
-	
+
 	@BeforeTest
 	public void setup() {
 		driverSetup = new DriverScript();
-		
-	}
-	@Test
-	public void draganddrop()
-	{
-		page = new DragandDropCheck(driverSetup.getDriver(), driverSetup.getBaseURL());
-		Actions act = new Actions(driverSetup.getDriver());
-		//act.moveToElement(page.getElementA()).clickAndHold(page.getElementA()).moveToElement(page.getElementB()).perform();
-		//act.moveToElement(page.getElementB(), 100, 25).perform();
-		act.dragAndDropBy(page.getElementA(), 200, 0).build().perform();
-		//act.dragAndDrop(page.getElementA(),page.getElementB()).build().perform();
-		
-		Assert.assertEquals(page.getElementA().getText(), "B");
-		
-		/* Action dragAndDrop =
-				 * act.clickAndHold(page.getElementA()).moveToElement(page.getElementB())
-				 * .release(page.getElementB()).build();
-				 * 
-				 * dragAndDrop.perform();
-				 */
 
 	}
-	
-	
-	/*
-	 * @AfterTest public void teardown() {
-	 * 
-	 * driverSetup.getDriver().close(); }
-	 */
+
+	@Test
+	public void draganddrop() {
+		page = new DragandDropCheck(driverSetup.getDriver(), driverSetup.getBaseURL());
+		Actions act = new Actions(driverSetup.getDriver());
+		
+		
+		act.dragAndDrop(page.getElementA(),page.getElementB()).build().perform();
+
+		Assert.assertEquals(page.getElementA().getText(), "B", " Failed to Drag and Drop");
+
+		/*
+		 * Action dragAndDrop =
+		 * act.clickAndHold(page.getElementA()).moveToElement(page.getElementB())
+		 * .release(page.getElementB()).build();
+		 * 
+		 * dragAndDrop.perform();
+		 */
+
+	}
+
+	@AfterTest
+	public void teardown() {
+
+		driverSetup.getDriver().close();
+	}
+
 }
