c466260c27dd430b958df28d6f31ce7e122fd1cb
sandeep-palnb <sandeep-palnb@gmail.com>
2023-02-13 09:42:24 -0500

    Adding FormAuthentication tests

--- src/test/java/com/dotdash/webtest/DotDashTests.java
+++ src/test/java/com/dotdash/webtest/DotDashTests.java
@@ -1,12 +1,39 @@
 package com.dotdash.webtest;
 
 import com.dotdash.webtest.pages.*;
+import com.dotdash.webtest.utils.ConfigReader;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class DotDashTests extends TestBase {
 
-    @Test(priority = 1)
+    @Test (priority = 1)
+    public void positiveLoginTest() {
+        FormAuthenticationPage formAuthenticationPage = new FormAuthenticationPage(driver);
+        formAuthenticationPage.goToFormAuthenticationPage();
+        formAuthenticationPage.login(ConfigReader.readProperty("username"), ConfigReader.readProperty("password"));
+        Assert.assertEquals(formAuthenticationPage.getSuggestionMessage(), "Welcome to the Secure Area. When you are done click logout below.");
+        formAuthenticationPage.clickLogout();
+    }
+
+    @Test(priority = 2, dataProvider = "loginData")
+    public void negativeLoginTest(String username, String password, String message) {
+        FormAuthenticationPage formAuthenticationPage = new FormAuthenticationPage(driver);
+        formAuthenticationPage.goToFormAuthenticationPage();
+        formAuthenticationPage.login(username, password);
+        Assert.assertTrue(formAuthenticationPage.getErrorMessage().contains(message));
+    }
+
+    @DataProvider(name = "loginData")
+    public Object[][] loginData() {
+        return new Object[][] {
+                {"pal", "SuperSecretPassword!", "Your username is invalid!"},
+                {"tomsmith", "007", "Your password is invalid!"}
+        };
+    }
+
+    @Test(priority = 3)
     public void checkBoxesTest() {
         CheckboxesPage checkboxesPage = new CheckboxesPage(driver);
         checkboxesPage.goToCheckboxPage();
@@ -16,7 +43,7 @@ public class DotDashTests extends TestBase {
         Assert.assertFalse(checkboxesPage.isCheckbox2Selected());
     }
 
-    @Test(priority = 2)
+    @Test(priority = 4)
     public void contextMenuTest() {
         ContextMenuPage contextMenuPage = new ContextMenuPage(driver);
         contextMenuPage.goToContextMenuPage();
