0703b1f4c43ed87ba2a2943e3e16a4fc6bf84d96
Kavyashreedani <129991353+Kavyashreedani@users.noreply.github.com>
2023-04-08 12:46:21 +0530

    Add files via upload

---
+++ src/test/testcases/DynamicLoadingPageTest.java
@@ -0,0 +1,40 @@
+package com.framework.testcases;
+import com.framework.pages.DynamicLoadingPage;
+import com.framework.testbase.TestBase;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class DynamicLoadingPageTest extends TestBase {
+    DynamicLoadingPage dynamicLoadingPage;
+
+    @BeforeTest
+    public void setup() {
+        super.setup();
+    }
+
+
+    @Test(priority = 1, description = "User is on the dynamic loading page")
+    public void user_is_on_dynamic_loading_page() {
+        dynamicLoadingPage = new DynamicLoadingPage(driver);
+        dynamicLoadingPage.navigate();
+        String actualURL = dynamicLoadingPage.getCurrentURL();
+        String expectedURL = "http://localhost:7080/dynamic_loading/2";
+        Assert.assertEquals(actualURL, expectedURL);
+    }
+
+    @Test(priority = 2, description = "User can wait for message to appear")
+    public void user_can_get_message_after_click() {
+        dynamicLoadingPage = new DynamicLoadingPage(driver);
+        dynamicLoadingPage.navigate().clickStart();
+        String actualMessage = dynamicLoadingPage.getMessage();
+        String expectedMessage = "Hello World!";
+        Assert.assertEquals(actualMessage, expectedMessage);
+    }
+
+    @AfterTest
+    public void teardown() {
+        super.cleanup();
+    }
+}
