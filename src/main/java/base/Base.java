3a2031e66149c77c0f1ff570e81370ae4c42e519
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2023-04-11 22:44:39 -0400

    dynamic loading test done

--- src/main/java/base/Base.java
+++ src/main/java/base/Base.java
@@ -48,7 +48,7 @@ public class Base {
 
     @BeforeMethod
     @Parameters({"browser", "URL"})
-    public void setDriver(@Optional("chrome") String browser, @Optional("http://localhost:7080/dynamic_controls") String URL) {
+    public void setDriver(@Optional("chrome") String browser, @Optional("http://localhost:7080/dynamic_loading/2") String URL) {
         initDriver(browser);
         driver.get(URL);
         driver.manage().window().maximize();
