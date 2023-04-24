7b74ee105285624322fa2563a18b67e2d0940004
Biowens <kjuri27@yahoo.com>
2023-04-24 14:10:43 -0400

    Fixed bug where "downloads" folder name string needed to be capitalized, Changed to DirectoryStream from listFiles

--- src/pages/FileDownloadPage.java
+++ src/pages/FileDownloadPage.java
@@ -1,21 +1,18 @@
 package pages;
 
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.io.File;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import  java.nio.file.Path;
 import java.time.Duration;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class FileDownloadPage extends BasePage {
     private String path = "/download";
 
     private By fileDownloadLinkBy = By.xpath("//a[contains(text(), 'some-file.txt')]");
-    private String downloadPath = System.getProperty("user.home") + File.separator + "downloads";
+    private String downloadPath = System.getProperty("user.home") + File.separator + "Downloads";
     private String fileName = "some-file";
     private String fileExtension = ".txt";
 
@@ -35,15 +32,31 @@ public class FileDownloadPage extends BasePage {
     }
 
     public boolean fileDownloaded(long downloadStartTime) {
-        File[] files = new File(downloadPath).listFiles();
+        // Initialize path for download location
+        Path dir = Paths.get(downloadPath);
 
-        for (File file : files) {
-            if (file.getName().contains(fileName) && file.getName().contains(fileExtension)
-                    && file.lastModified() > downloadStartTime && !fileName.contains("crdownload")) {
-                downloadedFile = file;
-                return true;
+        try {
+            DirectoryStream<Path> dirStream = Files.newDirectoryStream(dir);
+
+            for (Path entry: dirStream) {
+                File file = entry.toFile();
+                // Verify the file contains the file name, the appropriate extension,
+                // Was modified after the download was started,
+                // and does not contain crdownload, which meanas download is still in progress
+                if (file.getName().contains(fileName) && file.getName().contains(fileExtension)
+                        && file.lastModified() > downloadStartTime && !fileName.contains("crdownload")) {
+                    // Keep track of the downloaded file
+                    downloadedFile = file;
+                    return true;
+                }
             }
+
+        } catch (Exception e) {
+            // If there was an exception, most likely something failed and
+            // the file won't be found
+            return false;
         }
+        // If it never returned true, it did not find the file
         return false;
     }
 
